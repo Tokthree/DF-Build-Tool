0ffd235f70002afee8aacdc9ae2c8fc099c75fbd
Tokthree <TokthreeKF@gmail.com>
2025-02-06 22:46:45 +0000

    Migrated to Kivy for rendering, implemented basic baseline interface elements

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from window import Window
+import kivy
+kivy.require("2.1.0")
 
-def main():
-    win = Window(1920, 1080)
-    win.wait_for_close()
+from kivy.config import Config
+Config.set('graphics', 'width', '1920')
+Config.set('graphics', 'height', '1080')
 
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+
+class BuildScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(BuildScreen, self).__init__(**kwargs)
+
+
+class BuildApp(App):
+    def build(self):
+        self.title = 'Dead Frontier Build Tool'
+        return BuildScreen()
+        
 
 if __name__ == "__main__":
-    main()
+    BuildApp().run()
